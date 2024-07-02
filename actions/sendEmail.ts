61071e3f7d9ffbbfbff9e5e8cc4a48561a9112fa
Ratish Udawat <ratishudawat@gmail.com>
2024-07-02 12:17:16 +0530

    custom email & custom domain added

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,7 +1,9 @@
 'use server';
 
+import React from 'react';
 import { Resend } from 'resend';
 import { getErrorMessage, validateEmail } from '@/lib/utils';
+import ContactFormEmail from '@/email/contact-form-email';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -23,11 +25,14 @@ export const sendEmail = async (formData: FormData) => {
 
   try {
     await resend.emails.send({
-      from: 'Contact Form<onboarding@resend.dev>',
-      to: 'ratishudawat@gmail.com',
+      from: 'Contact Form<work@ratishu.com>',
+      to: 'work@ratishu.com',
       subject: 'Message from contact form',
       reply_to: senderEmail as string,
-      text: message as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      }),
     });
   } catch (error) {
     return { error: getErrorMessage(error) };
---
+++ email/contact-form-email.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Html, Body, Head, Heading, Hr, Container, Preview, Section, Text } from '@react-email/components';
+import { Tailwind } from '@react-email/tailwind';
+
+type ConactFormEmailProps = {
+  message: string;
+  senderEmail: string;
+};
+
+export default function ContactFormEmail({ message, senderEmail }: ConactFormEmailProps) {
+  return (
+    <Html>
+      <Head />
+      <Preview> New Message from portfolio site</Preview>
+      <Tailwind>
+        <Body>
+          <Container>
+            <Section>
+              <Heading>You received the following message from the contact form</Heading>
+              <Text>{message}</Text>
+              <Hr />
+              <Text>The sender's email is: {senderEmail}</Text>
+            </Section>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
