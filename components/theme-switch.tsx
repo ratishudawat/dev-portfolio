02b91e8ecb5780454a477bdc6c1d371e61d45a76
Ratish Udawat <ratishudawat@gmail.com>
2024-07-02 17:43:11 +0530

    bug fix, theme switch handler

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -8,8 +8,11 @@ export default function ThemeSwitch() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950'>
-      {theme === 'light' ? <BsSun onClick={toggleTheme} /> : <BsMoon onClick={toggleTheme} />}
+    <button
+      className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950'
+      onClick={toggleTheme}
+    >
+      {theme === 'light' ? <BsSun /> : <BsMoon />}
     </button>
   );
 }
