23e66bb978e3db9f4d5831c44e3543165dd4deaf
Ratish Udawat <ratishudawat@gmail.com>
2024-07-03 23:16:09 +0530

    picture updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import React from 'react';
-import img from '@/public/img.png';
+import img from '@/public/profile-picture.png';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
